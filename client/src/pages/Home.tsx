4af47694dc5ba85bfa43f5f503e5862853f95d54
svetlyakovdaniil-pixel <svetlyakovdaniil@gmail.com>
2026-04-04 01:14:57 +0700

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../../_core/hooks/useAuth";
+import { useAuth } from "../_core/hooks/useAuth";
 import { getLoginUrl } from "../../const";
 import { Button } from "../../components/ui/button";
 import { Badge } from "../../components/ui/badge";
